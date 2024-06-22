2ed4406c86f7f90671098441e3354d5ab15cc6b8
ElCenorove <dacruzsilvadavilucas96@gmail.com>
2024-06-22 18:35:29 -0300

    primeiro commit

---
+++ app/controllers/control.js
@@ -0,0 +1,110 @@
+const tarefasModel = require("../models/models");
+const moment = require("moment");
+const { body, validationResult } = require("express-validator");
+const auth = require('./../auth'); // Modúlo de gerenciamento de autenticação
+
+const TarefasControl = {
+
+CriarUsuario: async (req, res) => {
+ const errors = validationResult(req);
+ if (!errors.isEmpty()) {
+ console.log(errors);
+return res.render("pages/template-home", {
+ dados: req.body,
+ listaErros: errors,
+  pagina: "cadastro",
+  logado: null
+
+ });
+ }
+    try {
+  const resultados = await tarefasModel.create(req.body)
+ res.render("pages/template-home", { pagina: "home", logado: null, });
+
+ } catch (error) {
+   return error;
+     }
+  },
+
+   regrasValidacao: [
+     body("nome")
+     .isLength({ min: 3, max: 45 })
+  .withMessage("Nome invalido "),
+
+  body("email")
+    .isEmail()
+  .withMessage("Email invalido "),
+
+  body("senha")
+   .isLength({ min: 8, max: 30 })
+    .withMessage("senha invalido, deve conter pelo menos 8 digitos "),
+
+ body("c-senha")
+   .notEmpty()
+   .withMessage('Campo vazio.')
+        .custom((value, { req }) => {
+       const senha = req.body.senha
+     if (value != senha) {
+     throw new Error('Senha diferentes.')
+       }
+     return true;
+     })
+   ],
+ LoginUsuario: async (req, res) => {
+  const errors = validationResult(req);
+ if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.render("pages/template-home", {
+     dados: req.body,
+     listaErros: errors.array(),
+     pagina: "login",
+      logado: null
+    });
+  }
+
+     try {
+      const { email, senha } = req.body;
+      const user = await tarefasModel.authenticate(email, senha);
+
+        if (!user) {
+        return res.render("pages/template-home", {
+       mensagem: "Credenciais inválidas",
+     pagina: "login",
+   logado: null
+  });
+}
+
+      
+ auth.authenticateUser(user.id, user); // Guardas os usuários autenticado
+
+  res.render("pages/template-home", { pagina: "home", logado: user });
+ } catch (error) {
+ console.error('Erro no login:', error);
+return res.render("pages/template-home", {
+mensagem: "Erro no login",
+pagina: "login",
+logado: null
+  });
+ }
+},
+
+    // Defini as regras para efetuar o Loign de Forma Correta
+regrasValidacaoLogin: [
+body("email").isEmail().withMessage("Email inválido"),
+body("senha").notEmpty().withMessage("Senha não pode estar vazia")
+  ],
+
+LogoutUsuario: async (req, res) => {
+try {
+const userId = req.session.user.id; 
+auth.logoutUser(userId);
+
+res.redirect('/');
+} catch (error) {
+console.error('Erro no logout:', error);
+res.redirect('/'); 
+        }
+    }
+};
+
+module.exports = TarefasControl;
---
+++ app/models/models.js
@@ -0,0 +1,105 @@
+var pool = require("../../config/pool_conexoes")
+const bcrypt = require('bcrypt');
+const saltRounds = 10; 
+
+const tarefasModel = {
+    create: async (data) => {
+  try {
+  
+const hashedPassword = await bcrypt.hash(data.senha, saltRounds);
+
+ const [linhas] = await pool.query(
+ 'INSERT INTO usuario (`nome_usuario`, `email_usuario`, `senha_usuario`) VALUES (?, ?, ?)',
+   [data.nome, data.email, hashedPassword]
+  );
+
+return linhas;
+ } catch (error) {
+   throw error; 
+    }
+  },
+
+
+ findByEmail: async (email) => {
+  try {
+  const [rows] = await pool.query('SELECT * FROM usuario WHERE email_usuario = ?', [email]);
+  return rows[0]; 
+  } catch (error) {
+    console.error('Erro ao buscar usuário por email:', error);
+        throw error;
+     }
+ },
+
+ totalReg: async () => {
+ try {
+   const [linhas] = await pool.query('select count(*) FROM tipo_quartos WHERE status_quarto = 1;')
+  return linhas;
+ } catch (error) {
+ return error;
+ }  
+ },
+
+ findPage: async (pagina, total) => {
+   try {
+      const [linhas] = await pool.query('select * from tipo_quartos where status_quarto = 1 limit ?, ?', [pagina,total])
+      return linhas;
+     } catch (error) {
+      return error;
+  }  
+ },
+    
+update: async (camposForm) => {
+    try {
+const [resultados] = await pool.query(
+"UPDATE usuario SET nome_usuario = ?, user_usuario = ?, senha_usuario = ?,  " +
+ " email_usuario = ?, fone_usuario = ?, tipo_usuario = ?, status_usuario = ? " +
+   " WHERE id_usuario = ?",
+    [camposForm.nome_usuario, camposForm.user_usuario, camposForm.senha_usuario,
+     camposForm.email_usuario, camposForm.fone_usuario, camposForm.tipo_usuario,
+      camposForm.status_usuario, camposForm.id_usuario]
+      )
+       return resultados;
+     } catch (error) {
+    console.log(error);
+      return error;
+   }
+},
+
+delete: async (id) => {
+   try {
+  const [resultados] = await pool.query(
+     "UPDATE usuario SET status_usuario = 0 WHERE id_usuario = ? ", [id]
+       )
+      return resultados;
+     } catch (error) {
+ console.log(error);
+  return error;
+  }
+ },
+
+authenticate: async (email, senha) => {
+   try {
+const [rows] = await pool.query('SELECT * FROM usuario WHERE email_usuario = ?', [email]);
+const user = rows[0];
+
+  if (!user) {
+ return null; // Não foi possivel encontra o Usuário
+ }
+
+ // Verifica se a senha é válida 
+ const match = await bcrypt.compare(senha, user.senha_usuario);
+
+  if (match) {
+      return user; 
+ } else {
+ return null; // Senha incorreta
+ }
+} catch (error) {
+ throw error; 
+     }
+ },
+    
+  
+};
+
+module.exports = tarefasModel;
